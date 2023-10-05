231519ed50e92e9bb26b8376dc6a26c327ede803
Basil Barnaby <basil.barnaby@devmassive.com>
2023-10-05 08:59:47 -0400

    Naviagtion Menu View Component

---
+++ Components/ViewComponents/NavigationMenuViewComponent.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+using WorldDominion.Models;
+
+namespace WorldDominion.Components.ViewComponents
+{
+	public class NavigationMenuViewComponent : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			var menuItems = new List<MenuItem>
+			{
+				new MenuItem{ Controller = "Home", Action = "Index", Label = "Home" },
+				new MenuItem{ Controller = "Departments", Action = "Index", Label = "Departments" },
+				new MenuItem{ Controller = "Products", Action = "Index", Label = "Products" },
+				new MenuItem{ Controller = "Home", Action = "Privacy", Label = "Privacy" },
+			};
+
+			return View(menuItems);
+		}
+	}
+}
